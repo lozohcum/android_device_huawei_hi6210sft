7efe95cd4fc4c0085b58935f8a2538f2f9338c0d
hak86 <hakamelassouad@gmail.com>
2017-05-08 10:41:54 +0200

    hi6210sft: add languages_full

--- device_hi6210sft.mk
+++ device_hi6210sft.mk
@@ -3,4 +3,5 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
