b8e9d13113b787ca019279b9731428b1a15a1004
hak86 <hakamelassouad@gmail.com>
2017-05-08 10:41:41 +0200

    hi6210sft: add full_base_telephony

--- device_hi6210sft.mk
+++ device_hi6210sft.mk
@@ -2,4 +2,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
