507daeb9387930e6d47af7466812e70d7db21a1b
hak86 <hakamelassouad@gmail.com>
2017-05-08 10:42:15 +0200

    hi6210sft: build charger_res_images

--- device_hi6210sft.mk
+++ device_hi6210sft.mk
@@ -5,3 +5,5 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+PRODUCT_PACKAGES += \
+    	charger_res_images \
